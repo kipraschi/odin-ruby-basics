85f4be5e83122fc14f2326b79bfacdc07c7d048f
Kipras Chi <kipraschi@gmail.com>
2026-01-24 18:04:25 +0200

    refactor: change ascii numbers to letter ordinal calls

--- caesar-cipher/caesar-cipher.rb
+++ caesar-cipher/caesar-cipher.rb
@@ -1,20 +1,18 @@
 def caesar_cipher(string, shift_factor)
   string
   .chars
-  .map do |letter|
-    ascii_code = letter.ord
-    if ascii_code.between?(65, 90)
-      base = 65
-    elsif ascii_code.between?(97, 122) 
-      base = 97
+  .map do |letter| 
+    case letter.ord
+    when 'A'.ord..'Z'.ord
+      base = 'A'.ord
+    when 'a'.ord..'z'.ord
+      base = 'a'.ord
     else
       next letter
     end
 
-    encrypted_ascii = base + (ascii_code - base + shift_factor) % 26
+    encrypted_ascii = base + (letter.ord - base + shift_factor) % 26
     encrypted_ascii.chr
   end
   .join("")
 end
-
-p caesar_cipher("What a string!", 5)
