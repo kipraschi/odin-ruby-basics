3b4bb4b0eab682e22b1324f35e465b1f4559d9f1
Kipras Chi <kipraschi@gmail.com>
2026-01-24 17:53:21 +0200

    fix: address cases with large shift factors and negative shift factors

--- caesar-cipher/caesar-cipher.rb
+++ caesar-cipher/caesar-cipher.rb
@@ -1,18 +1,20 @@
 def caesar_cipher(string, shift_factor)
-  def encrypt(letter, shift_factor)
-    ascii_code = letter.ord
-    shifted_ascii = ascii_code + shift_factor
-
-    shifted_ascii -= 26 if # wrap from z to a
-    ((ascii_code >= 65 && ascii_code <= 90) && shifted_ascii > 90 ) || # uppercase 65-90
-    ((ascii_code >= 97 && ascii_code <= 122) && shifted_ascii > 122 ) # lowercase 97-122
-
-    shifted_ascii.chr
-  end
-
   string
-  .split("")
-  .map { |char| char.match?(/[[:alpha:]]/) ? encrypt(char, shift_factor) : char}
-  .join("")
+  .chars
+  .map do |letter|
+    ascii_code = letter.ord
+    if ascii_code.between?(65, 90)
+      base = 65
+    elsif ascii_code.between?(97, 122) 
+      base = 97
+    else
+      next letter
+    end
 
+    encrypted_ascii = base + (ascii_code - base + shift_factor) % 26
+    encrypted_ascii.chr
+  end
+  .join("")
 end
+
+p caesar_cipher("What a string!", 5)
